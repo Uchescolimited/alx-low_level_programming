10f573221c4164a16b8103d58b0c1e5a06aa207c
Uchescolimited <saintsixtus4000@gmail.com>
2022-07-04 16:18:10 -0700

    My thirteenth commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int left;
+	int right;
+	int center;
+
+	for (left =48; left <= 57; left++)
+	{
+		for (center + left +
