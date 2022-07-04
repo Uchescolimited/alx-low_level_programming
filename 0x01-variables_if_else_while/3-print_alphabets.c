a15f1881484c41780905f7d23ea72f2d0a8a7da3
Uchescolimited <saintsixtus4000@gmail.com>
2022-07-04 14:31:34 -0700

    My fourth commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,8 +12,7 @@ int main(void)
 	{
 		putchar(lower_case);
 	}
-
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++);
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
 		putchar(upper_case);
 	}
