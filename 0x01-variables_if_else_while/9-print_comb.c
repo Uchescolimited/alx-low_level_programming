bf0706be5364aaff740aba57a7177ca6a180bfc8
Uchescolimited <saintsixtus4000@gmail.com>
2022-07-04 15:33:15 -0700

    My tenth commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit <= 57; digit++)
+	{
+		putchar(digit);
+		if (digit == 57)
+		{
+			break;
+		}
+		putchar(',');
+		putchar (' ');
+	}
+	putchar('\n');
+	return (0);
+}
