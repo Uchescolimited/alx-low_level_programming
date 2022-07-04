fa3ba4050c8386442d79c9341dbc362bf4e58654
Uchescolimited <saintsixtus4000@gmail.com>
2022-07-04 16:34:34 -0700

    My twelfth commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,6 +9,25 @@ int main(void)
 	int right;
 	int center;
 
-	for (left =48; left <= 57; left++)
+	for (left = 48; left <= 57; left++)
 	{
-		for (center + left +
+		for (center = left + 1; center <= 57; center++)
+		{
+			for (right = center + 1; right <= 57; right++)
+			{
+				putchar (left);
+				putchar (center);
+				putchar (right);
+
+				if ((left == 55) && (center == left + 1) && (right == center + 1))
+				{
+					break;
+				}
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
